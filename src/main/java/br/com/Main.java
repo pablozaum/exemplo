56076d6370f666d3d3a1ca1b17a5b7603c87c51b
unknown <Administrador@L4MAQ15.labcesac.net>
2019-10-10 22:00:33 -0300

    a

---
+++ src/main/java/br/com/Main.java
@@ -0,0 +1,12 @@
+package br.com;
+
+public class Main {
+
+    public static void main(final String[] arg) {
+        System.out.println("Oi");
+    }
+    
+     public static  boolean validar(){
+        return true;
+    }
+}
